832cb326a51bfb8f77d0dbc73c4486b7a1b8abc0
mhmae <mhmae@unsitra.fr>
2026-01-09 08:48:43 +0100

    💄 reorganize CSS files and clean up unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,63 @@
-import './App.css'
-import { useState } from 'react'
+import './assets/App.css'
+// import { useState } from 'react'
 
 function App() {
-  const displayImpact = () => {
-      setIsImpact(true)
-      setTimeout(() => {
-        setIsImpact(false)
-      }, 500)
-    }
-
-  const onClick = async () => {
-    await displayImpact()
-
-    await displayHitBody()
-
-    await displayBackgroundBody()
-
-    await displayBodyComeBack()
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  const sprites = [
-    '../public/Celle-la_nobg4bits.png',
-    '../public/Celle-la_nobg4bits.png',
-    '../public/Celle-la_nobg4bits.png',
-  ]
-
-  const [isImpact, setIsImpact] = useState<boolean>(true)
-  const [imageDisplayed, setImageDisplayed] = useState<string>('../public/Celle-la_nobg4bits.png')
-
+  // const displayImpact = () => {
+  //     setIsImpact(true)
+  //     setTimeout(() => {
+  //       setIsImpact(false)
+  //     }, 500)
+  //   }
+  //
+  // const onClick = async () => {
+  //   await displayImpact()
+  //
+  //   await displayHitBody()
+  //
+  //   await displayBackgroundBody()
+  //
+  //   await displayBodyComeBack()
+  // }
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  // const sprites = [
+  //   '../public/default.png',
+  //   '../public/default.png',
+  //   '../public/default.png',
+  // ]
+  //
+  // const [isImpact, setIsImpact] = useState<boolean>(true)
+  // const [imageDisplayed, setImageDisplayed] = useState<string>('../public/default.png')
+  //
 
 
   return (
     <>
       <div className="container">
-        <img
-          className="image-me"
-          src={imageDisplayed}
-          alt="test"
-          onClick={displayImpact}
-        />
-        {isImpact && (
-          <img
-            className="image-impact"
-            src="../public/impact_8bit.png"
-            alt="test"
-          />
-        )}
+        {/*<img*/}
+        {/*  className="image-me"*/}
+        {/*  src={imageDisplayed}*/}
+        {/*  alt="test"*/}
+        {/*  onClick={displayImpact}*/}
+        {/*/>*/}
+        {/*{isImpact && (*/}
+        {/*  <img*/}
+        {/*    className="image-impact"*/}
+        {/*    src="../public/impact.png"*/}
+        {/*    alt="test"*/}
+        {/*  />*/}
+        {/*)}*/}
       </div>
     </>
   )
