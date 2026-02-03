e8e1daafa4fce341b85da72e676d5d5d8c9444bc
mhmae <mhmae@unsitra.fr>
2026-02-03 11:01:31 +0100

    🌐 Add i18n config files & json

---
+++ src/plugins/i18n.ts
@@ -0,0 +1,31 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import translationEN from '../../public/locales/en/translation.json'
+import translationFR from '../../public/locales/fr/translation.json'
+
+export const availableLanguages = {
+  en: {
+    translation: translationEN,
+    flag: '🇬🇧',
+  },
+  fr: {
+    translation: translationFR,
+    flag: '🇫🇷',
+  },
+}
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: availableLanguages,
+    fallbackLng: 'en', // fallback language if translation is not found for the current language
+    lng: localStorage.getItem('lang') || 'en',
+    supportedLngs: ['en', 'fr'],
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
+
+export default i18n
